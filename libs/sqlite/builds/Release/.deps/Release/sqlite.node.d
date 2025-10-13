cb87ff9ee556f077df35ae40c784a3ede0350dd5
KobraRocks <buddy@kobra.rocks>
2025-10-13 11:06:59 +0200

    renamed builds to build

--- libs/sqlite/builds/Release/.deps/Release/sqlite.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/sqlite.node := ln -f "Release/obj.target/sqlite.node" "Release/sqlite.node" 2>/dev/null || (rm -rf "Release/sqlite.node" && cp -af "Release/obj.target/sqlite.node" "Release/sqlite.node")
